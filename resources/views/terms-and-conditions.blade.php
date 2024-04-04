ca82781025878502c6c03967c8041aa41cd08c3c
Lucas Siqueira <lucas@lsiqueira.dev>
2024-04-04 18:13:36 -0300

    feat: add pages policy

---
+++ resources/views/terms-and-conditions.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+            theme: {
+                fontFamily: {
+                    'sans': 'Kanit, Helvetica, Arial, sans-serif',
+                }
+            }
+            }
+        </script>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+
+                    <h2 class="text-3xl font-semibold text-black text-center dark:text-white">Termos e Condições</h2>
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NewPasswordController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/privacy-policy', function () { return view('privacy-policy'); });
+Route::get('/terms-and-conditions', function () { return view('terms-and-conditions'); });
 
 Route::get('changed-password', [NewPasswordController::class, 'index'])->name('password.changed');
 Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
